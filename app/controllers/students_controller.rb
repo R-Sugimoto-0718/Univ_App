b6c0ee4813bd0ffe23818b6d5313eb3b23116333
waspryo <wasp2123321@gmail.com>
2019-12-29 23:27:42 +0900

    DRY your code - extract redundancies

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,14 @@
 class StudentsController < ApplicationController
+    # DRY,メソッド内で同じ記述を使う場合はprivate直下に共通のメソッドを定義し
+    # before_actionでprivateのメソッドを書きonlyでどのメソッドだけに入らせるかを書くだけ
+    before_action :set_student, only: [:show, :edit, :update]
+
     def index
         @students = Student.all
     end
 
     def show
-        @student = Student.find(params[:id])
+
     end 
 
     def new
@@ -15,18 +19,17 @@ class StudentsController < ApplicationController
         @student = Student.new(student_params)
         if @student.save
             flash[:notice] = "You have successfully signed up"
-            redirect_to root_path
+            redirect_to @student
         else
             render 'new'
         end
     end
 
     def edit
-        @student = Student.find(params[:id])
+
     end
 
     def update
-        @student = Student.find(params[:id])
         if @student.update(student_params)
             flash[:notice] = 'You have successfully updated your profile'
             # redirect_to student_path(@student)または
@@ -38,6 +41,10 @@ class StudentsController < ApplicationController
     end
 
     private
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
 
     def student_params
         params.require(:student).permit(:name, :email)
