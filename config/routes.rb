b401672545d7c82d6d97aa51cc67cf6a2d8b1b35
waspryo <wasp2123321@gmail.com>
2019-12-29 16:35:05 +0900

    Students index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  # resourcesは全てのアクションルートを出してくれる
+  # execpt [:dest]はデストロイ以外をだすメソッド
+  resources :students, except: [:destroy]
 end
